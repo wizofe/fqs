46297b6d7842c610ee3376eeaae3fbde2f69da93
Ioannis Valasakis <code@wizofe.uk>
2022-05-20 15:50:56 +0200

    Add the filtering code

---
+++ src/filtering.jl
@@ -0,0 +1,8 @@
+function filter_bp_fmri(fmri, low, high):
+    # fmri: 4D time series
+    # low: lower cutoff frequency for bandpass filter
+    # high: upper cutoff frequency for bandpass filter
+
+    # returns save the NIFTI locally appending a _bp
+    # inherits scipy.signal butter filtfilt (Butterworth)
+end
